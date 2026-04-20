30f071510da74951671ed0a8804a8e7dcace0fea
FurkanBayrak20240602013 <furkan.bayrak313@gmail.com>
2026-04-20 16:15:57 +0300

    Add CoachDTO class for coach data transfer

---
+++ src/main/java/com/lambda/sports/persistence/CoachDTO.java
@@ -0,0 +1,11 @@
+package com.lambda.sports.persistence;
+
+/** Flat data-transfer object representing a coach on disk. */
+public class CoachDTO {
+
+    public String name;
+    public int    age;
+    public double trainingSkill;
+
+    public CoachDTO() {}
+}
